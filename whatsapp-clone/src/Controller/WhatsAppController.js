d880929b9febf3aa180eb9be38e206593bb61224
Erik Sidnei Correa <erik@562@hotmail.com>
2020-01-18 01:04:39 -0300

    Adicionando eventos no botões de perfil e de nova mensagem

--- whatsapp-clone/src/Controller/WhatsAppController.js
+++ whatsapp-clone/src/Controller/WhatsAppController.js
@@ -3,6 +3,7 @@ class WhatsAppController {
     console.log('WhatsAppController ok');
     this.elementsPrototype();
     this.loadElements();
+    this.initEvents();
 
   }
   loadElements() {
@@ -57,5 +58,34 @@ class WhatsAppController {
       return this.classList.contains(name);
     }
 
+  }
+  initEvents() {
+    this.el.myPhoto.on('click', e => {
+      this.closeAllLeftPanel();
+      this.el.panelEditProfile.show();
+      setTimeout(() => {
+        this.el.panelEditProfile.addClass('open');
+      },300);
+    });
+
+    this.el.btnNewContact.on('click', e => {
+      this.closeAllLeftPanel();
+      this.el.panelAddContact.show();
+      setTimeout(()=> {
+        this.el.panelAddContact.addClass('open');
+      },300);
+    });
+
+    this.el.btnClosePanelEditProfile.on('click', e => {
+      this.el.panelEditProfile.removeClass('open')
+    });
+
+    this.el.btnClosePanelAddContact.on('click', e => {
+      this.el.panelAddContact.removeClass('open');      
+    });
+  }
+  closeAllLeftPanel() {
+    this.el.panelAddContact.hide();
+    this.el.panelEditProfile.hide();
   }
 }
