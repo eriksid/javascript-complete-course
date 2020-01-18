88cb9be5adc54cbcbe60ebe8986af473a29d0b7f
Erik Sidnei Correa <erik@562@hotmail.com>
2020-01-17 23:21:16 -0300

    Adiciondo html css audio e Criando controller e pegando elementos com id e transformando em camel case

---
+++ whatsapp-clone/src/Controller/WhatsAppController.js
@@ -0,0 +1,12 @@
+class WhatsAppController {
+  constructor () {
+    console.log('WhatsAppController ok');
+    this.loadElements();
+  }
+  loadElements() {
+    this.el = {};
+    document.querySelectorAll('[id]').forEach(element => {
+      this.el[Format.getCamelCase(element.id)] = element;
+    })
+  }
+}
